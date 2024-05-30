a52dbd707bf462065b14c9ed32e16d505e8bd063
kunkyab <tenzinkunkyab5@gmail.com>
2024-05-30 13:59:23 -0400

    changes in func.js
    added canvas  and background

---
+++ func.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("board");
+const ctx = canvas.getContext("2d");
+
+const GAME_WIDTH = 1250;
+const GAME_HEIGHT = 700;
+canvas.height = GAME_HEIGHT;
+canvas.width = GAME_WIDTH;
